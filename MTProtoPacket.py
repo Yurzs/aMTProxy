aae20c9d4940c80c94574365165050e7e04c8e83
Yurzs <yurzs@icloud.com>
2018-05-27 19:42:07 +0300

    Update MTProtoPacket.py

--- MTProtoPacket.py
+++ MTProtoPacket.py
@@ -1,127 +1,55 @@
 from hashlib import sha256
 import numpy
-from AESEncryptor import AESencryptor
-
+from AESEncryptor import AESencryptor,AESencryptorCTR
+import os
 class MTProxy():
     class MTProtoPacket():
         def __init__(self):
-            self.encryptCountBuf = bytearray()
-            self.decryptCountBuf = bytearray()
-            self.encryptNum = 0
-            self.decryptNum = 0
+            pass
 
-        class ProtocolType():
-            none = 0
-            AbridgedObfuscated2 = 1
-            IntermediateObfuscated2 = 2
+        def obfuscated2(self,raw_data):
+            obf_enc_key_bytes = os.urandom(64)
+            obf_enc_key = obf_enc_key_bytes[7:39]  # 8 - 39 bytes [32]
+            obf_enc_iv = obf_enc_key_bytes[39:55]  # 40 - 55 bytes [16]
+            encryptor = AESencryptorCTR(key=obf_enc_key,
+                                        iv=obf_enc_iv,
+                                        counter=0,
+                                        number=0)
+            enc_data = encryptor.encrypt(raw_data)
 
-        def EncryptObfuscated2(self,data,length):
-            return self.AesCtr128Encrypt(data,length)
+            return obf_enc_key_bytes + enc_data
 
-        def DecryptObfuscated2(self,buffer, length):
-            return self.AesCtr128Decrypt(buffer, length)
+        def deobfuscated2(self,enc_data):
+            obf_dec_key_bytes = bytes(enc_data[0:64])[::-1]
+            obf_dec_key = obf_dec_key_bytes[7:39]  # 8 - 39 bytes [32]
+            obf_dec_iv = obf_dec_key_bytes[39:55]  # 40 - 55 bytes [16]
+            encryptor = AESencryptorCTR (key=obf_dec_key,
+                                         iv=obf_dec_iv,
+                                         counter=0,
+                                         number=0)
+            raw_data = encryptor.decrypt(enc_data[64:])
+            return raw_data
 
-        def AesCtr128Encrypt(self,buffer,length):
-            output = bytearray(length=length)
-            for i in range(length):
-                if self.encryptNum == 0:
-                    ecountBuf = 'aa' ###TODO ADD NUMPY LAST BLOCK TRANSFORMATION
-                    self.ivec = reversed(self.ivec)
-                    bigInteger = int(numpy.concatenate,bytearray({ 0x00 })) +1
-                    bigIntegerArray = bytearray(bigInteger)
-                    self.ivec = bigIntegerArray[0:min(len(self.ivec),len(bigIntegerArray))]
-                    self.ivec = reversed(self.ivec)
-                output[i] = bytearray(buffer[i] ^ self.encryptCountBuf[self.encryptNum])
-                number = (self.encryptNum + 1) % 16;
-            return output
+        def serverside_deobfuscated2(self, enc_data):
+            obf_dec_key = enc_data[7:39]  # 8 - 39 bytes [32]
+            obf_dec_iv = enc_data[39:55]  # 40 - 55 bytes [16]
+            encryptor = AESencryptorCTR(key=obf_dec_key,
+                                        iv=obf_dec_iv,
+                                        counter=0,
+                                        number=0)
+            raw_data = encryptor.decrypt(enc_data[64:])
+            return raw_data
 
-        def AesCtr128Decrypt(self,buffer,length):
-            output = bytearray (length=length)
-            for i in range (length):
-                if self.decryptNum == 0:
-                    ecountBuf = 'aa' ###TODO ADD NUMPY LAST BLOCK TRANSFORMATION
-                    ivdc = reversed (self.ivdc)
-                    bigInteger = int (numpy.concatenate, bytearray ({0x00})) + 1
-                    bigIntegerArray = bytearray (bigInteger)
-                    self.ivdc = bigIntegerArray[0:min (len (self.ivdc), len (bigIntegerArray))]
-                    self.ivdc = reversed (self.ivdc)
-                output[i] = bytearray (buffer[i] ^ self.decryptCountBuf[self.decryptNum])
-                number = (self.decryptNum + 1) % 16;
-            return output
+        def serverside_obfuscated2(self, raw_data):
+            obf_enc_key_bytes = os.urandom (64)
+            obf_enc_key = (obf_enc_key_bytes[7:39])  # 8 - 39 bytes [32]
+            obf_enc_iv = (obf_enc_key_bytes[39:55])
+            obf_enc_key_bytes = obf_enc_key_bytes[::-1]
 
-        def SetInitBufferObfuscated2(self, buffer, secret):
-            reversed_buffer = buffer[8:48]
-            reversed_buffer = reversed (reversed_buffer)
-            key = buffer[8:32]
-            key_reversed = reversed_buffer[0:32]
-            hex_secret = bytearray.fromhex (secret.decode ("hex"))
-            key = sha256 (numpy.concatenate(key,hex_secret))
-            key_reversed = sha256(numpy.concatenate(key_reversed,hex_secret))
+            encryptor = AESencryptorCTR (key=obf_enc_key,
+                                         iv=obf_enc_iv,
+                                         counter=0,
+                                         number=0)
+            enc_data = encryptor.encrypt (raw_data)
 
-            self.encrypt_key = key_reversed
-            self.encryptIv = reversed_buffer[32:16]
-            self.decrypt_key = key
-            self.decryptIv = buffer[40:16]
-
-            self.cryptoTransformEncrypt = AESencryptor(key=self.encrypt_key)
-            self.cryptoTransformDecrypt = AESencryptor(key=self.decrypt_key)
-
-            decrypted_buffer = self.DecryptObfuscated2(buffer, len(buffer))
-            for i in range(len(decrypted_buffer)):
-                if i > 55:
-                    continue
-                buffer[i] = decrypted_buffer[i]
-
-            protocol_result = buffer[56:-4]
-            if protocol_result[0] == 0xef and\
-               protocol_result[1] == 0xef and\
-               protocol_result[2] == 0xef and\
-               protocol_result[3] == 0xef:
-                self.protocol_type = self.ProtocolType.AbridgedObfuscated2
-            elif protocol_result[0] == 0xee and\
-                 protocol_result[1] == 0xee and\
-                 protocol_result[2] == 0xee and\
-                 protocol_result[3] == 0xee:
-                self.protocol_type = self.ProtocolType.IntermediateObfuscated2
-            else:
-                self.protocol_type = self.ProtocolType.none
-
-        def GetInitBufferObfuscated2(self,protocol_type):
-            self.protocol_type = protocol_type
-            buffer = bytearray(length=64)
-            done = False
-            while not done:
-                buffer = numpy.random.random(64)
-                val = (buffer[3] << 24) | (buffer[2] << 16) | (buffer[1] << 8) | (buffer[0])
-                val2 = (buffer[7] << 24) | (buffer[6] << 16) | (buffer[5] << 8) | (buffer[4])
-                if buffer[0] != 0xef and val != 0x44414548 and val != 0x54534f50 and val != 0x20544547 and val != 0x4954504f and val2 != 0x00000000:
-                    if protocol_type == 1:
-                        buffer[56] = buffer[57] = buffer[58] = buffer[59] = 0xef
-                        done = True
-                    if protocol_type == 2:
-                        buffer[56] = buffer[57] = buffer[58] = buffer[59] = 0xee
-                        done = True
-                    if protocol_type == 0:
-                        return None
-                break
-            keyIvEncrypt = buffer[8:48]
-            self.encryptKey = keyIvEncrypt[0:32]
-            self.encryptIv = keyIvEncrypt[32:16]
-
-            keyIvEncrypt = reversed(keyIvEncrypt)
-            self.decryptKey = keyIvEncrypt[0:32]
-            self.decryptIv = keyIvEncrypt[32:16]
-
-            self.cryptoTransformEncrypt = AESencryptor(self.encryptKey)
-            self.cryptoTransformDecrypt = AESencryptor(self.decryptKey)
-
-            encrypted_buffer = self.EncryptObfuscated2(buffer, len(buffer))
-            for i in range(len(encrypted_buffer)):
-                if i > 55:
-                    continue
-                buffer[i] = encrypted_buffer[i]
-            return buffer
-
-
-
-
+            return obf_enc_key_bytes + enc_data
